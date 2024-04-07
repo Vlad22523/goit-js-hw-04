f31ad763e4ecfa57f9f8be46430f58abd8359eba
Vlad Melnyk <116571536+Vlad22523@users.noreply.github.com>
2024-04-07 04:05:29 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+let filterArray = (numbers, value) => {
+  const arr = [];
+  for (const ch of numbers) {
+    if (ch > value) {
+      arr.push(ch);
+    }
+  }
+  return arr;
+};
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
